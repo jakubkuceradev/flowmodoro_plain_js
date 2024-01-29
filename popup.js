cddceee46c609d28efc0b36b5b79eea2ca93c011
Jakub Kucera <kucerj64@fit.cvut.cz>
2024-01-29 17:42:06 +0100

    Move app logic to background.js. Make popup.js request data.

--- popup.js
+++ popup.js
@@ -8,100 +8,36 @@ const COLORPAUSED = "#ffff00";
 const COLORWORK = "#ff0000";
 const COLORBREAK = "#00ff2f";
 
-const audio = new Audio("sounds/alarm.wav");
-
-let breakTimeDivisor = 4; // Used to determine the length of a break.
-let milisecondsTime = 0;
-let currentStatus = "paused";
-
 document.addEventListener("DOMContentLoaded", () => {
     const buttonElement = document.getElementById("work-break");
 
-    // pauseTimer();
-    renderStatus();
-    renderTime();
+    chrome.runtime.sendMessage({ action: "requestData" }, (response) => {
+        if (response) console.log(response);
 
-    window.addEventListener("pagehide", () => {
-        document.title = `INACTIVE ${formatTime(minutesTime)}:${formatTime(secondsTime)}`;
+        if (response && response.time && response.status) {
+            console.log("response:", response, response.time, response.status);
+            renderTime(response.time);
+            renderStatus(response.status);
+        }
     });
 
-    buttonElement.addEventListener("click", (event) => {
-        if (event.type !== "click") {
-        } else if (currentStatus === "paused") {
-            startWork();
-        } else if (currentStatus === "work") {
-            startBreak();
-        } else if (currentStatus === "break") {
-            pauseTimer();
+    chrome.runtime.onMessage.addListener((request) => {
+        if (request.action === "updateData") {
+            console.log("received updateData message from background.js in popup.js");
+            renderTime(request.time);
+            renderStatus(request.status);
         }
+    });
+
+    buttonElement.addEventListener("click", () => {
+        chrome.runtime.sendMessage({ action: "handleButtonClick" });
     });
 });
 
-// Resets and pauses the timer.
-const pauseTimer = () => {
-    currentStatus = "paused";
-    milisecondsTime = 0;
-
-    renderTime();
-    renderStatus();
-};
-
-// Starts the timer.
-const startWork = () => {
-    currentStatus = "work";
-    milisecondsTime = 0;
-
-    audio.pause();
-    renderTime();
-    renderStatus();
-
-    const startTime = Date.now();
-
-    const updateTime = () => {
-        const currentTime = Date.now();
-        milisecondsTime = currentTime - startTime;
-
-        if (currentStatus !== "work") return;
-
-        renderTime();
-        setTimeout(updateTime, 200);
-    };
-
-    updateTime();
-};
-
-// Calculates the time and starts the break count down.
-const startBreak = () => {
-    currentStatus = "break";
-    milisecondsTime = Math.floor(milisecondsTime / breakTimeDivisor);
-
-    renderTime();
-    renderStatus();
-
-    const startTime = Date.now();
-    const breakTime = milisecondsTime;
-
-    const updateTime = () => {
-        const currentTime = Date.now();
-        const elapsedTime = currentTime - startTime;
-        milisecondsTime = breakTime - elapsedTime;
-
-        if (milisecondsTime <= 0) {
-            audio.play();
-
-            pauseTimer();
-        }
-        if (currentStatus !== "break") return;
-
-        renderTime();
-        setTimeout(updateTime, 200);
-    };
-
-    updateTime();
-};
-
 // Updates the shown status message and the color of the timer.
-const renderStatus = () => {
+const renderStatus = (currentStatus) => {
+    console.log("rendering status", "status:", currentStatus);
+
     const color = (() => {
         if (currentStatus === "paused") return COLORPAUSED;
         else if (currentStatus === "work") return COLORWORK;
@@ -114,12 +50,14 @@ const renderStatus = () => {
         else if (currentStatus === "break") return STATUSBREAKMESSAGE;
     })();
 
+    console.log("setting color to", color, "in popup.js");
+
     document.documentElement.style.setProperty("--color-secondary", color);
     document.getElementById("status-message").textContent = statusMessage;
 };
 
 // Updates the shown time.
-const renderTime = () => {
+const renderTime = (milisecondsTime) => {
     const minutesTime = Math.floor(milisecondsTime / (1000 * 60));
     const secondsTime = Math.floor(milisecondsTime / 1000 - minutesTime * 60);
 
